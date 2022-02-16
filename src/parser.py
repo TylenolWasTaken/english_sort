44ec6923599b0e2c2586f4829bd40b27862be9f9
Jayden Huntley <TylenolWasTaken@protonmail.com>
2022-02-16 13:46:46 -0500

    Initial commit

---
+++ src/parser.py
@@ -0,0 +1,15 @@
+import json as js
+
+
+# Setup Json to append to
+words = dict({"word": []})
+
+# File formatting
+resources_dir: str = "resources"
+with open(f'{resources_dir}/words_alpha.txt', 'r') as stage1:
+    Lines = stage1.readlines()
+    for line in Lines:
+        words['word'].append(line.strip())
+
+with open(f'{resources_dir}/words.json', 'w') as f:
+    js.dump(words, f)
